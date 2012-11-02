6ab19969f823aeb8c0bc835c2f10d553e70bdef1
Erik Gustavson <erik@3ptsoft.com>
2012-11-02 15:39:57 -0700

    added debug output

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -37,6 +37,9 @@ RSpec.configure do |config|
   #     --seed 1234
   config.order = "random"
   
+  Capybara.register_driver :poltergeist do |app|
+    Capybara::Poltergeist::Driver.new(app, :debug => true)
+  end
   
   Capybara.javascript_driver = :poltergeist
 end

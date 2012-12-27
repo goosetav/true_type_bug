3f91db1b43641e8641d437624d2a842484f020a0
Erik Gustavson <erik@3ptsoft.com>
2012-12-26 16:59:01 -0800

    make it even simpler

--- crash-phantom.js
+++ crash-phantom.js
@@ -1,22 +1,11 @@
 var url = 'http://true_type_bug.dev';
-var viewport = {
-  width: 1024,
-  height: 768
-}
-
 
 var page = require('webpage').create();
-page.viewportSize = viewport;
-
 
 page.open(url, function () {
     
-  // reset process
   page.release();
-  phantom.clearCookies();
   page = require('webpage').create();
-  page.viewportSize = viewport;    
-  
   
   page.open(url, function () {
     phantom.exit();
